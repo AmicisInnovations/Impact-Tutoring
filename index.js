42803207c8b907625b3909050a9b9c2da48be3c8
Dylan-Naidoo <dylannaidoo2004@gmail.com>
2026-01-21 18:00:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("appnow")?.addEventListener("click", () => {
         window.location.href = "apply.html";
     });
+    document.getElementById("appnow1")?.addEventListener("click", () => {
+        window.location.href = "apply.html";
+    });
+    document.getElementById("appnow2")?.addEventListener("click", () => {
+        window.location.href = "apply.html";
+    });
+    document.getElementById("appnow3")?.addEventListener("click", () => {
+        window.location.href = "apply.html";
+    });
+    document.getElementById("appnow4")?.addEventListener("click", () => {
+        window.location.href = "apply.html";
+    });
+    document.getElementById("appnow5")?.addEventListener("click", () => {
+        window.location.href = "apply.html";
+    });
 
     document.getElementById("tutors")?.addEventListener("click", () => {
         window.location.href = "tutors.html";
